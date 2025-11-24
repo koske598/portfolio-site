011e80822ba07160794d9f2010c5639183d19e6d
kokuba <118956372+koske598@users.noreply.github.com>
2025-11-24 16:54:53 +0900

    Add skills component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { About } from "./pages/about/about";
+import { Skills } from "./pages/skills/skills";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, About],
+  imports: [Header, Footer, About, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
