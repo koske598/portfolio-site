c9e4da1d0e86ad22ee32be9790413b195d421754
kokuba <118956372+koske598@users.noreply.github.com>
2025-11-24 15:53:03 +0900

    Add about component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { About } from "./pages/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    Header,
-    Footer,
-  ],
+  imports: [RouterOutlet, Header, Footer, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
