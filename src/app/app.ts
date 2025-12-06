9a458b5bfd77b25a16e7f1e674232639cc05d08c
kokuba <118956372+koske598@users.noreply.github.com>
2025-12-06 16:29:52 +0900

    feat: add projects component selector to app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { About } from "./pages/about/about";
 import { Skills } from "./pages/skills/skills";
+import { Projects } from "./pages/projects/projects";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, About, Skills],
+  imports: [Header, Footer, About, Skills, Projects],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
