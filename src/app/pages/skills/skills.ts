70be43d54e67b66dbf2e0fec3a9933751956fdbc
kokuba <118956372+koske598@users.noreply.github.com>
2025-12-06 17:36:31 +0900

    refactor: simplify template using @for

--- src/app/pages/skills/skills.ts
+++ src/app/pages/skills/skills.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+export type Skill = {
+  skillNo: number,
+  skillImage: string,
+  imageAlt: string,
+  skillText: string
+}
 
 @Component({
   selector: 'app-skills',
@@ -8,4 +16,30 @@ import { Component } from '@angular/core';
 })
 export class Skills {
 
+  skills: Skill[] = [
+    {
+      skillNo: 1,
+      skillImage: 'assets/TypeScript.svg',
+      imageAlt: 'TypeScript',
+      skillText: 'TypeScript'
+    },
+    {
+      skillNo: 2,
+      skillImage: 'assets/Angular.svg',
+      imageAlt: 'Angular',
+      skillText: 'Angular'
+    },
+    {
+      skillNo: 3,
+      skillImage: 'assets/Java.png',
+      imageAlt: 'Java',
+      skillText: 'Java'
+    },
+    {
+      skillNo: 4,
+      skillImage: 'assets/Spring.svg',
+      imageAlt: 'Spring Boot',
+      skillText: 'Spring Boot'
+    }
+  ];
 }
